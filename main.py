7958273826294c09d1b3d81e2a4af1c8fbb1f95f
Andrew Friedman <andrewfriedman01@gmail.com>
2023-04-19 03:52:19 -0400

    Add spotify functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import base64
+import requests
 from collections import defaultdict
 from config import YOUTUBE_API_KEY, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
 
@@ -35,14 +37,80 @@ def get_video_ids(api_key, playlist_id):
 
 def get_video_titles(api_key, video_ids):
     base_url = 'https://www.googleapis.com/youtube/v3/videos'
-    params = {
-        'part': 'snippet',
-        'id': ','.join(video_ids),
-        'maxResults': 100,
-        'key': api_key
+    video_titles = []
+    
+    for i in range(0, len(video_ids), 50):
+        batch_video_ids = video_ids[i:i + 50]
+        params = {
+            'part': 'snippet',
+            'id': ','.join(batch_video_ids),
+            'maxResults': 50,
+            'key': api_key
+        }
+        response = requests.get(base_url, params=params)
+        if response.status_code != 200:
+            print(f"Error fetching video details: {response.status_code} - {response.text}")
+            return []
+        response_json = response.json()
+        if 'items' not in response_json:
+            print("Error: 'items' not found in the API response")
+            return []
+        video_titles.extend([item['snippet']['title'] for item in response_json['items']])
+    
+    return video_titles
+
+def get_spotify_access_token(client_id, client_secret):
+    auth_url = 'https://accounts.spotify.com/api/token'
+    auth_data = {
+        'grant_type': 'client_credentials'
     }
-    response = requests.get(base_url, params=params).json()
-    return [item['snippet']['title'] for item in response['items']]
+    auth_header = {
+        'Authorization': 'Basic ' + base64.b64encode(f'{client_id}:{client_secret}'.encode('utf-8')).decode('utf-8')
+    }
+    response = requests.post(auth_url, data=auth_data, headers=auth_header)
+
+    if response.status_code != 200:
+        response_json = response.json()
+        if response.status_code == 400 and response_json.get('error') == 'invalid_client':
+            print("Invalid client. Please check your client_id and client_secret.")
+        else:
+            print(f"Error obtaining Spotify access token: {response.status_code} - {response.text}")
+        return None
+
+    return response.json()['access_token']
+
+
+def get_genres_for_track(spotify_access_token, track_title):
+    search_url = 'https://api.spotify.com/v1/search'
+    search_params = {
+        'q': track_title,
+        'type': 'track',
+        'limit': 1
+    }
+    search_header = {
+        'Authorization': f'Bearer {spotify_access_token}'
+    }
+    response = requests.get(search_url, params=search_params, headers=search_header)
+    if response.status_code != 200:
+        print(f"Error searching for track: {response.status_code} - {response.text}")
+        return []
+
+    search_result = response.json()
+    if search_result.get('tracks', {}).get('items'):
+        track = search_result['tracks']['items'][0]
+        artist_id = track['artists'][0]['id']
+    else:
+        print("Track not found on Spotify")
+        return []
+
+    artist_url = f'https://api.spotify.com/v1/artists/{artist_id}'
+    response = requests.get(artist_url, headers=search_header)
+    if response.status_code != 200:
+        print(f"Error fetching artist details: {response.status_code} - {response.text}")
+        return []
+
+    artist = response.json()
+    return artist['genres']
 
 def main():
     playlist_url = input("Enter a YouTube Music playlist URL: ")
@@ -50,37 +118,33 @@ def main():
 
     if not playlist_id:
         print("Invalid URL")
-    else:
-        video_ids = get_video_ids(YOUTUBE_API_KEY, playlist_id)
-        if len(video_ids) == 0:
-            return
+        return
+    
+    video_ids = get_video_ids(YOUTUBE_API_KEY, playlist_id)
+    if len(video_ids) == 0:
+        return
 
-        video_titles = get_video_titles(YOUTUBE_API_KEY, video_ids)
+    video_titles = get_video_titles(YOUTUBE_API_KEY, video_ids)
 
-        # This part requires you to integrate with a music metadata API, like Spotify.
-        # You can follow the Spotify API documentation to implement this part.
-        # https://developer.spotify.com/documentation/web-api/
+    genre_count = defaultdict(int)
+    total_genres = 0
 
-        # For demonstration purposes, let's assume you have a function called
-        # `get_genres_for_track` that takes a track title and returns a list of genres.
-        # def get_genres_for_track(spotify_access_token, track_title): ...
+    spotify_access_token = get_spotify_access_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+    if spotify_access_token is None:
+        return
 
-        genre_count = defaultdict(int)
-        total_genres = 0
+    for title in video_titles:
+        genres = get_genres_for_track(spotify_access_token, title)
+        for genre in genres:
+            genre_count[genre] += 1
+            total_genres += 1
 
-        for title in video_titles:
-            genres = get_genres_for_track(spotify_access_token, title)
-            for genre in genres:
-                genre_count[genre] += 1
-                total_genres += 1
+    ranked_genres = sorted(genre_count.items(), key=lambda x: x[1], reverse=True)
 
-        ranked_genres = sorted(genre_count.items(), key=lambda x: x[1], reverse=True)
-
-        print("Genre percentages for the playlist:")
-        for genre, count in ranked_genres:
-            percentage = (count / total_genres) * 100
-            print(f"{genre}: {percentage:.2f}%")
-
+    print("Genre percentages for the playlist:")
+    for genre, count in ranked_genres:
+        percentage = (count / total_genres) * 100
+        print(f"{genre}: {percentage:.2f}%")
 
 if __name__ == "__main__":
     main()
